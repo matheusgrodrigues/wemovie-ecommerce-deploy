78c4d1fb9fbf2679fc722ef4597823f1a9b80b13
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2024-11-19 15:39:33 -0300

    refact: refatorando promise e separando no service

---
+++ src/services/MovieService.ts
@@ -0,0 +1,21 @@
+import { MovieResponse } from "../schema/MovieSchema";
+
+const MovieService = {
+   getMovies: async (): Promise<MovieResponse> => {
+      try {
+         const res = await fetch(`${process.env.REACT_APP_PUBLIC_DOMAIN}/data/movies.json`, {
+            headers: {
+               "Content-Type": "application/json",
+            },
+         });
+
+         if (!res.ok) throw new Error(`Erro ao buscar os filmes: ${res.status}`);
+
+         return res.json();
+      } catch (error) {
+         throw new Error("Erro ao buscar os filmes");
+      }
+   },
+};
+
+export default MovieService;
